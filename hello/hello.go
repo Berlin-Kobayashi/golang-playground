18c9a0f09ed58891f65da2852c14eb54489ab06d
Daniel Schulz <daniel.schulz@fashionforhome.de>
2015-12-15 11:45:22 +0100

    - added sample library - added sample test

---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DanShu93/golang-playground/stringutil"
+//	"github.com/golang/example/stringutil"
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("Hello GO :)"))
+}
