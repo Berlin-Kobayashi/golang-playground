74ebbdf5d87359b53547025a1d216366a0b2bca9
Daniel Schulz <daniel.schulz@ocono.de>
2017-01-28 13:18:55 +0100

    Add ansi racing game

--- ansi/ansi.go
+++ ansi/ansi.go
@@ -1,29 +1,59 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
+	"math/big"
+	"os"
+	"os/exec"
+	"regexp"
+	"strconv"
 	"time"
 )
 
 func main() {
-	for i := 30; i < 38; i++ {
-		fmt.Println(color(i, "hello"))
-		fmt.Println(bold(color(i, "hello")))
+	clearTerminal()
+	chars := []string{"X", "Y", "Z"}
+	width := getTerminalWidth()
+	for num := 0; ; num++ {
+		for i, char := range chars {
+			steps, _ := rand.Int(rand.Reader, big.NewInt(5))
+			for j := int64(0); j < steps.Int64(); j++ {
+				if len(chars[i]) >= width {
+					fmt.Println(strconv.Itoa(i) + " WON!")
+					return
+				}
+				chars[i] = " " + char
+			}
+
+			resetCursor()
+			for _, printedChar := range chars {
+
+				// for colorCode := 30; colorCode < 38; colorCode++ {
+				fmt.Println(bold(color(31+i%6, printedChar)))
+				// }
+			}
+			time.Sleep(time.Millisecond * 10)
+		}
 	}
-	fmt.Print("Hollaa")
-	time.Sleep(time.Second * 1)
-	fmt.Print("\rasdasda")
-	fmt.Print("\n")
-	time.Sleep(time.Second * 1)
+}
+
+func getTerminalWidth() int {
+	cmd := exec.Command("stty", "size")
+	cmd.Stdin = os.Stdin
+	out, _ := cmd.Output()
+	widthRegex := regexp.MustCompile(".* (.*)\n")
+	width, _ := strconv.Atoi(string(widthRegex.ReplaceAll(out, []byte("$1"))))
+	return width
+}
+
+func clearTerminal() {
 	fmt.Print("\033[2J")
 	fmt.Print("\033[H")
-	fmt.Println("test")
-	fmt.Println("test2")
-    time.Sleep(time.Second * 1)
+}
+
+func resetCursor() {
 	fmt.Printf("\033[0;0H")
-	fmt.Println("testy")
-    fmt.Println("test2")
-    time.Sleep(time.Second * 1)
 }
 
 func bold(input string) string {
