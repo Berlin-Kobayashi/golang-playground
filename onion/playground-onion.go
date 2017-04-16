601d61a505a9bc53482a1d2a1c919df96c7ed848
Daniel Schulz <daniel.schulz@ocono.de>
2017-04-16 20:27:32 +0200

    Experiment with onion maker kit

--- onion/playground-onion.go
+++ onion/playground-onion.go
@@ -7,12 +7,37 @@ import (
 	"fmt"
 	"flag"
 	"os/exec"
+	"strings"
+	"errors"
 )
 
 var gpioPinToStatus = map[int]bool{0: false, 1: false, 2: false, 3: false, 18: false, 19: false}
 
+var sevenSegmentDigitPins = []int{11, 18, 19, 0}
+
 func main() {
-	useShiftRegister()
+	useSevenSemgentDisplay()
+}
+
+// Experiment 6
+func useSevenSemgentDisplay() {
+	clearSevenSegmentDisplay()
+
+	flag.Parse()
+
+	input := flag.Args()[0]
+
+	characters := strings.Split(input, "")
+	if len(characters) != 4 {
+		panic(errors.New("4 characters needed!"))
+	}
+
+	for {
+		for i, character := range characters {
+			writeCharacterToSevenSegmentDisplay(i, character[0])
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
 }
 
 // Experiment 5
@@ -83,6 +108,48 @@ func blinkLED() {
 		time.Sleep(time.Second)
 		setGPIO(0, true)
 	}
+}
+
+func writeCharacterToSevenSegmentDisplay(index int, character byte) {
+	digitPin := sevenSegmentDigitPins[index]
+
+	setGPIO(digitPin, false)
+	setShiftRegister(1, 2, 3, getSevenSegmentByteCodeForCharacter(character))
+	setGPIO(digitPin, true)
+}
+
+func clearSevenSegmentDisplay() {
+	setShiftRegister(1, 2, 3, "00000000")
+
+	for _, digitPin := range sevenSegmentDigitPins {
+		setGPIO(digitPin, false)
+		setGPIOOut(digitPin)
+	}
+}
+
+func getSevenSegmentByteCodeForCharacter(character byte) string {
+	characterToByteCode := map[byte]string{
+		'0': "11111100",
+		'1': "01100000",
+		'2': "11011010",
+		'3': "11110010",
+		'4': "01100110",
+		'5': "10110110",
+		'6': "10111110",
+		'7': "11100000",
+		'8': "11111110",
+		'9': "11110110",
+		'a': "11101110",
+		'b': "00111110",
+		'c': "10011100",
+		'd': "01111010",
+		'e': "10011110",
+		'f': "10001110",
+		' ': "00000000",
+		'-': "00000010",
+	}
+
+	return characterToByteCode[character]
 }
 
 func setShiftRegister(serialDataPin, serialClockPin, registerClockPin int, byteString string) {
